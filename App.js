f223c35ad9d68e066d665d83a1e61c41fbf9f9dc
codekane <ryan.horricks@gmail.com>
2021-05-13 06:34:06 -0600

    Preparing for commit to Github. Sure hope there's nothing sensitive in here. If the camera's are watching -- HI MOM!

---
+++ App.js
@@ -0,0 +1,12 @@
+function App() {
+
+  return(
+    <div className="App">
+      <h1 className="stonkk-header">Stonkk</h1>
+    </div>
+
+  )
+
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+function StonkQuote(data) {
+  const symbol = () => { return Object.keys(data.data)[0] }
+  const parsed_data = data.data[symbol()];
+
+  return(
+    <div className="stonk-query container">
+      <div className="stonk-query-top row">
+        <div className="col perfect-center item">
+          <h2>{symbol()}</h2>
+        </div>
+        <div className="col perfect-center item">
+          <b>{parsed_data.bid}</b>
+        </div>
+
+        <div className="col perfect-center item">
+          <p>{parsed_data.regularMarketChange.toFixed(2)} ({parsed_data.regularMarketChangePercent.toFixed(2)})</p>
+        </div>
+
+      </div>
+      <div className="stonk-query-bottom perfect-center">
+        <p>{parsed_data.shortName}</p>
+      </div>
+    </div>
+
+  )
+}
+
+function StonkSummary(data) {
+  const symbol = () => { return Object.keys(data.data)[0] }
+  const parsed_data = data.data[symbol()];
+
+  const dayHigh = () => {
+    console.log(parsed_data);
+    return parsed_data.dayHigh;
+  }
+
+  const open = () => {
+    console.log(parsed_data.open);
+  }
+
+
+  return (
+    <div className="stonkk-container">
+      <div className="stonk">
+        <h2>${symbol()}</h2>
+        <div className="stonkk-info">
+          <p>Open: {parsed_data.open}</p>
+          <p>Close: {parsed_data.bid}</p>
+          <p>Low: {parsed_data.dayLow}</p>
+          <p>High: {parsed_data.dayHigh}</p>
+        </div>
+      </div>
+    </div>
+  )
+
+}
 
 function App() {
-  return (
+  const [data, setData] = React.useState([]);
+  const [dataKeys, setDataKeys] = React.useState([])
+
+  const getSummary = async () => {
+    const api_url = 'http://localhost:9292/api/quotes'
+    let response = await fetch(api_url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    let json = await response.json();
+    console.log(json);
+    setData(json);
+    return json;
+  }
+
+  const parseSummary = (arr) => {
+
+  }
+
+
+  useEffect(() => {
+    (async () => {
+      let res = await getSummary();
+      if (res.success) {
+         }
+    })();
+  }, [] )
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="stonkk-header">
+        <h1>Stonkk</h1>
       </header>
+
+      {data.map(d => (<StonkQuote key={Object.keys(d)[0]} data={d} />))}
     </div>
-  );
+
+  )
+
 }
 
 export default App;
